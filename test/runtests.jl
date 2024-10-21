ae9ca72178d7370660b8e6cc3d26a1a21f9ba841
Elias Carvalho <eliascarvdev@gmail.com>
2024-10-21 15:26:13 -0300

    Update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -40,66 +40,20 @@ using Test
     @test tree[1] == 11
     @test tree[3] == 33
 
-    # delete
-    tree = AVLTree{Int,Int}()
-    tree[2] = 20
-    tree[1] = 10
-    tree[3] = 30
-    delete!(tree, 3)
-    @test !isnothing(tree.root)
-    @test !isnothing(tree.root.left)
-    @test isnothing(tree.root.right)
-    delete!(tree, 1)
-    @test !isnothing(tree.root)
-    @test isnothing(tree.root.left)
-    @test isnothing(tree.root.right)
-    delete!(tree, 2)
-    @test isnothing(tree.root)
-
-    # tree that accept any types
-    tree = AVLTree()
-    tree[2] = 'A'
-    tree[1] = 1.1
-    tree[3] = "test"
-    @test tree[2] == 'A'
-    @test tree[1] == 1.1
-    @test tree[3] == "test"
-
-    # AbstractTrees interface
+    # right rotate
     tree = AVLTree{Int,Int}()
     tree[3] = 30
     tree[2] = 20
-    tree[4] = 40
     tree[1] = 10
-    tree[5] = 50
-    root = tree.root
-    @test children(root) === (root.left, root.right)
-    @test children(root.left) === (root.left.left,)
-    @test children(root.right) === (root.right.right,)
-    @test children(root.left.left) === ()
-    @test children(root.right.right) === ()
-    @test nodevalue(root) == 30
-    @test nodevalue(root.left) == 20
-    @test nodevalue(root.right) == 40
-    @test nodevalue(root.left.left) == 10
-    @test nodevalue(root.right.right) == 50
-    @test NodeType(root) === HasNodeType()
-    @test nodetype(root) === typeof(root)
+    @test tree.root.key == 2
+    @test tree.root.left.key == 1
+    @test tree.root.right.key == 3
 
     # left rotate
     tree = AVLTree{Int,Int}()
     tree[1] = 10
     tree[2] = 20
     tree[3] = 30
-    @test tree.root.key == 2
-    @test tree.root.left.key == 1
-    @test tree.root.right.key == 3
-
-    # right rotate
-    tree = AVLTree{Int,Int}()
-    tree[3] = 30
-    tree[2] = 20
-    tree[1] = 10
     @test tree.root.key == 2
     @test tree.root.left.key == 1
     @test tree.root.right.key == 3
@@ -121,6 +75,62 @@ using Test
     @test tree.root.key == 2
     @test tree.root.left.key == 1
     @test tree.root.right.key == 3
+
+    # delete
+    tree = AVLTree{Int,Int}()
+    tree[2] = 20
+    tree[1] = 10
+    tree[3] = 30
+    delete!(tree, 3)
+    @test !isnothing(tree.root)
+    @test !isnothing(tree.root.left)
+    @test isnothing(tree.root.right)
+    delete!(tree, 1)
+    @test !isnothing(tree.root)
+    @test isnothing(tree.root.left)
+    @test isnothing(tree.root.right)
+    delete!(tree, 2)
+    @test isnothing(tree.root)
+
+    # tree that accepts any types
+    tree = AVLTree()
+    tree[2] = 'A'
+    tree[1] = 1.1
+    tree[3] = "test"
+    @test tree[2] == 'A'
+    @test tree[1] == 1.1
+    @test tree[3] == "test"
+    delete!(tree, 3)
+    @test !isnothing(tree.root)
+    @test !isnothing(tree.root.left)
+    @test isnothing(tree.root.right)
+    delete!(tree, 1)
+    @test !isnothing(tree.root)
+    @test isnothing(tree.root.left)
+    @test isnothing(tree.root.right)
+    delete!(tree, 2)
+    @test isnothing(tree.root)
+
+    # AbstractTrees interface
+    tree = AVLTree{Int,Int}()
+    tree[3] = 30
+    tree[2] = 20
+    tree[4] = 40
+    tree[1] = 10
+    tree[5] = 50
+    root = tree.root
+    @test children(root) === (root.left, root.right)
+    @test children(root.left) === (root.left.left,)
+    @test children(root.right) === (root.right.right,)
+    @test children(root.left.left) === ()
+    @test children(root.right.right) === ()
+    @test nodevalue(root) == 30
+    @test nodevalue(root.left) == 20
+    @test nodevalue(root.right) == 40
+    @test nodevalue(root.left.left) == 10
+    @test nodevalue(root.right.right) == 50
+    @test NodeType(root) === HasNodeType()
+    @test nodetype(root) === typeof(root)
 
     # show
     tree = AVLTree{Int,Int}()
